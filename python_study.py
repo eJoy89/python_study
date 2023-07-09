05d1d7a237b6446ca091ad91cda48675f9b25421
ejoy89 <cho89@live.co.kr>
2023-07-09 12:40:09 +0900

    data

--- python_study.py
+++ python_study.py
@@ -1,10 +1,26 @@
-a = 'test'
-b = 777
-c = True
+a = "Life is too short, You need Python"
+b = a[5] + a[6]
+c = a[-6] + a[-5] + a[-4] + a[-3] + a[-2] + a[-1]
+d = a[8:11]
+e = a[0:]
 
-
-print(a)
+print(a[0], a[1], a[2], a[3])
 print(b)
 print(c)
+print(d)
+print(e)
 
-print(777 == b)
+
+format = "I eat %d apples." %7
+format1 = "I ate %s cherries" % "six"
+
+num = 7
+format2 = "I had %s melons" % num
+
+day = 'two'
+format3 = "I ate %d apples. so I was sick for %s days" %(num, day)
+
+print(format)
+print(format1)
+print(format2)
+print(format3)
