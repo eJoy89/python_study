716e2e84e4da5d11ed14e0a2080e940b46b2dd5a
ejoy89 <cho89@live.co.kr>
2023-07-30 21:39:04 +0900

    dictionary

--- python_tuple.py
+++ python_tuple.py
@@ -7,3 +7,25 @@ print(t1, t2, t3)
 
 print(t3)
 print(t3[1])
+
+
+# Dictionary
+
+dic = { 'name': 'Cho', 'phone': '010-xxxx-xxxx', 'birth' : '0912' }
+print(dic)
+
+print(dic['name'])
+print(dic['phone'])
+print(dic['birth'])
+print(dic.keys())
+
+
+a = {1: 'a'}
+a[2] = 'b'
+print(a)
+
+a['name'] = 'cho'
+print(a)
+
+del a['name']
+print(a)
