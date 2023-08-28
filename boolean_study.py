915ad07fb04a9de164fb529b534ab5e8f94d75ab
ejoy89 <cho89@live.co.kr>
2023-08-28 20:30:39 +0900

    if

--- boolean_study.py
+++ boolean_study.py
@@ -1,3 +1,16 @@
 a = [1, 2, 3, 4]
 while a:
     print(a.pop())
+    
+    
+if[1, 2, 3]:
+    print('true')
+else: 
+    print('false')
+    
+    
+    
+if[]:
+    print('true')
+else: 
+    print('false')
