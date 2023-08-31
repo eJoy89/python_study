c32890081b5663d5c2088557272d57dbfbf8a18b
ejoy89 <cho89@live.co.kr>
2023-08-31 21:14:16 +0900

    :

--- boolean_study.py
+++ boolean_study.py
@@ -1,32 +1,40 @@
-a = [1, 2, 3, 4]
-while a:
-    print(a.pop())
+# a = [1, 2, 3, 4]
+# while a:
+#     print(a.pop())
     
     
-if[1, 2, 3]:
-    print('true')
-else: 
-    print('false')
+# if[1, 2, 3]:
+#     print('true')
+# else: 
+#     print('false')
     
     
     
-if[]:
-    print('true')
-else: 
-    print('false')
+# if[]:
+#     print('true')
+# else: 
+#     print('false')
     
     
-bool([1, 2, 3])
-bool([])
-bool(0)
-bool(2)
-print('------------')
+# bool([1, 2, 3])
+# bool([])
+# bool(0)
+# bool(2)
+# print('------------')
 
 
-b = [1, 2, 3]
-print(id(b))
+# b = [1, 2, 3]
+# print(id(b))
 
-c = [1, 2, 3]
-d = c
-print(id(c))
-print(id(d))
+# c = [1, 2, 3]
+# d = c
+# print(id(c))
+# print(id(d))
+
+a = [1, 2, 3]
+# b = a[:]
+b = a
+
+a[1] = 4
+print(a)
+print(b)
