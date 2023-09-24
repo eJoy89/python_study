df1d4e495fdd2c22829c1833b0c529e4749b575d
ejoy89 <cho89@live.co.kr>
2023-09-24 12:30:40 +0900

    further Condition

--- conditional.py
+++ conditional.py
@@ -5,8 +5,40 @@
 #     print('walking')
 
 
-money = 1
-if money:
-    print('택시를')
-    print('타고')
-    print('가라')
+# money = 1
+# if money:
+#     print('택시를')
+#     print('타고')
+#     print('가라')
+    
+    
+# x = 3
+# y = 2
+
+# print(x > y)
+# print(x < y)
+# print(x == y)
+
+
+# money = 900
+# if money >= 1000:
+#     print('take a cap')
+    
+# else: 
+#     print('take a walk')
+
+money = 2000
+card = False
+if money > 2000 or card:
+    print('take a cap')
+else:
+    print('take a walk')
+    
+
+print(1 in [1, 2, 3])
+
+print(1 not in [1, 2, 3])
+
+
+print('a' in ['a', 'b', 'c'])
+print('a' not in ['a', 'b', 'c'])
