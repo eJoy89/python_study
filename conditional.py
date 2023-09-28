ee63736c9620bffc4b1dc89555c6af9a4cf39a1e
ejoy89 <cho89@live.co.kr>
2023-09-28 18:40:43 +0900

    if pass

--- conditional.py
+++ conditional.py
@@ -27,18 +27,27 @@
 # else: 
 #     print('take a walk')
 
-money = 2000
-card = False
-if money > 2000 or card:
-    print('take a cap')
-else:
-    print('take a walk')
+# money = 2000
+# card = False
+# if money > 2000 or card:
+#     print('take a cap')
+# else:
+#     print('take a walk')
     
 
-print(1 in [1, 2, 3])
+# print(1 in [1, 2, 3])
 
-print(1 not in [1, 2, 3])
+# print(1 not in [1, 2, 3])
 
 
-print('a' in ['a', 'b', 'c'])
-print('a' not in ['a', 'b', 'c'])
+# print('a' in ['a', 'b', 'c'])
+# print('a' not in ['a', 'b', 'c'])
+
+pocket = ['papper', 'money', 'cellphone']
+
+# if 'money' in pocket:
+if 'no money' in pocket:
+    pass
+
+else:
+    print('by Card')
