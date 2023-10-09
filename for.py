d98bfb57a4f2793ce60e03e5f0ef4a5f84e7ee4e
ejoy89 <cho89@live.co.kr>
2023-10-09 22:07:04 +0900

    for 2nd

--- for.py
+++ for.py
@@ -2,6 +2,20 @@
 # for i in test_list:
 #     print(i)
     
-a = [ (1, 2), (3, 4), (5, 6) ]
-for(frist, second) in a:
-    print(frist + second)
+# a = [ (1, 2), (3, 4), (5, 6) ]
+# for(frist, second) in a:
+#     print(frist + second)
+
+marks = [90, 25, 67, 45, 80]
+number = 0
+# for mark in marks:
+#     number = number + 1
+#     if mark >= 60:
+#         print('%d번 학생은 합격입니다' % number)
+#     else:
+#         print('%d번 학생은 불합격입니다' % number)
+        
+for mark in marks:
+    number = number + 1
+    if mark < 60: continue
+    print('%d번 학생 축하합니다. 합격입니다' % number)
