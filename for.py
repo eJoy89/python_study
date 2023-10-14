108d6496f6f89e888440f57505dfb64fa1717df6
ejoy89 <cho89@live.co.kr>
2023-10-15 00:58:58 +0900

    for and multiplication

--- for.py
+++ for.py
@@ -6,8 +6,8 @@
 # for(frist, second) in a:
 #     print(frist + second)
 
-marks = [90, 25, 67, 45, 80]
-number = 0
+# marks = [90, 25, 67, 45, 80]
+# number = 0
 # for mark in marks:
 #     number = number + 1
 #     if mark >= 60:
@@ -15,7 +15,21 @@ number = 0
 #     else:
 #         print('%d번 학생은 불합격입니다' % number)
         
-for mark in marks:
-    number = number + 1
-    if mark < 60: continue
-    print('%d번 학생 축하합니다. 합격입니다' % number)
+# for mark in marks:
+#     number = number + 1
+#     if mark < 60: continue
+#     print('%d번 학생 축하합니다. 합격입니다' % number)
+
+# a = range(10)
+# # print(list(a))
+
+# add = 0
+# for i in a:
+#     add = add + i
+# print(add)
+
+for i in range(2, 10):
+    for j in range(1, 10):
+        # print(i * j)
+        print(i*j, end= " ")
+    print('')
