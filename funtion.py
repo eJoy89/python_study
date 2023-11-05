29e5eb317a82f2cfb8c00765065c80bb853ec50d
ejoy89 <cho89@live.co.kr>
2023-11-05 18:55:41 +0900

    parameter

--- funtion.py
+++ funtion.py
@@ -1,13 +1,24 @@
+# def add(a, b):
+#     return a + b
+
+# print(add(7, 5))
+
+# a = add(3, 4)
+# print(a)
+
+# def say():
+#     return 'hi'
+
+# a = say()
+# print(a)
+
+# def sub(a, b):
+#     return a - b
+# print(sub(10, 3))
+
 def add(a, b):
     return a + b
 
-print(add(7, 5))
+result = add(a = 3, b = 5)
 
-a = add(3, 4)
-print(a)
-
-def say():
-    return 'hi'
-
-a = say()
-print(a)
+print(result)
