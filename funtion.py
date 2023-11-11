6aac84c15e33b0f1c149caa7cf6148905b2ad11c
ejoy89 <cho89@live.co.kr>
2023-11-12 01:41:50 +0900

    fn 12.11.2023

--- funtion.py
+++ funtion.py
@@ -16,9 +16,23 @@
 #     return a - b
 # print(sub(10, 3))
 
-def add(a, b):
-    return a + b
+# def add(a, b):
+#     return a + b
 
-result = add(a = 3, b = 5)
+# result = add(a = 3, b = 5)
 
+# print(result)
+
+def add_mul(choice, *args):
+    if choice == "add":
+        result = 0
+        for i in args:
+            result = result + i
+    elif choice == "mul":
+        result = 1
+        for i in args:
+            result = result + i
+    return result
+
+result = add_mul('add', 1,2,3,4,5)
 print(result)
