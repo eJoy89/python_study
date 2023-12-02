4965f6dd6eaa5a05df165052ce2e0b201efccf91
ejoy89 <cho89@live.co.kr>
2023-12-03 01:32:26 +0900

    input and print

---
+++ inputNoutput.py
@@ -0,0 +1,12 @@
+# number = input("숫자를 입력하세요: ")
+
+# print(number)
+
+# a = 123
+# print(a)
+
+# print("life" "is" "too short")
+# print("Life", "is", "too Short")
+
+for i in range(10):
+    print(i+1, end=",")
