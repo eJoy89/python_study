847388dfa56963f5fd03255b71a5c86b33111ad3
joy <cho89@live.co.kr>
2023-12-12 07:27:06 +0900

    read file

--- file.py
+++ file.py
@@ -9,8 +9,21 @@
 #     data = '%d번째 줄입니다\n' % i
 #     print(data)
 
-f = open('새파일1.txt', 'w')
-for i in range(1, 11):
-    data = "%d번쨰 줄 입니다.\n" % i
-    f.write(data)
+# f = open('새파일3.txt', 'w')
+# for i in range(1, 11):
+#     data = "이것은 %d번쨰 줄 입니다.\n" % i
+#     f.write(data)
+# f.close()
+
+
+# f = open('새파일3.txt', 'r')
+# line = f.readline()
+# print(line)
+# f.close()
+
+f = open('새파일3.txt', 'r')
+while True:
+    line = f.readline()
+    if not line: break
+    print(line)
 f.close()
