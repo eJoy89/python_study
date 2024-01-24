d538cc50b56cced78663d6a1d3a9c24a5a16899b
ejoy89 <cho89@live.co.kr>
2024-01-24 21:21:31 +0900

    api

--- api.py
+++ api.py
@@ -6,27 +6,27 @@ r = requests.get('https://httpbin.org/get', params=payload)
 
 print(r.url) 
 
-# import requests
-# url = "http://date.jsontest.com/"
+import requests
+url = "http://date.jsontest.com/"
 
 
-# response = requests.get(url)
-# response = requests.post(url)
-# response = requests.delete(url, data={'key':'value'})
-# response = requests.head(url)
-# response = requests.options(url)
+response = requests.get(url)
+response = requests.post(url)
+response = requests.delete(url, data={'key':'value'})
+response = requests.head(url)
+response = requests.options(url)
 
 
-# def get_json(url):
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         json_data = response.json()
-#         return json_data
-#     else:
-#         print("Error accessing the URL:", response.status_code)
-#         return None
+def get_json(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        json_data = response.json()
+        return json_data
+    else:
+        print("Error accessing the URL:", response.status_code)
+        return None
 
-# # Example usage
-# data = get_json(url)
-# if data:
-#     print(data)
+# Example usage
+data = get_json(url)
+if data:
+    print(data)
