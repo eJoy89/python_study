aa632b971e517f373fc83d5417a78c5748d2ba48
ejoy89 <cho89@live.co.kr>
2024-02-04 18:21:27 +0900

    dart api

---
+++ dart.py
@@ -0,0 +1,13 @@
+import dart_fss as dart
+
+api_key = '1b71ebc44b5343892c2b050c94121db21de94eb2'
+dart.set_api_key(api_key=api_key)
+
+corp_list = dart.get_corp_list()
+samsung = corp_list.find_by_corp_name('삼성전자',exactly=True)[0]
+
+fs = samsung.extract_fs(bgn_de='20120101')
+
+fs.save()
+
+print(samsung)
